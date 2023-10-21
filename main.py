2e7dc5a709b6c2e7c9a81859b11a1af86de9dd18
vortexsys <120136010+vortexsys@users.noreply.github.com>
2023-10-21 15:27:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,43 +8,55 @@ import ctypes
 from datetime import date
 
 #### Startup stuff
-ctypes.windll.kernel32.SetConsoleTitleW("Artemis v1| https://github.com/vortexsys/artemis")
+ctypes.windll.kernel32.SetConsoleTitleW("Artemis v1 | vortexsys")
 today = date.today()
+
 
 with open('./config.json') as f:
    config = json.load(f)
 
+### Definitions
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#### Standalone Code
 def restart():
-    clear()
     os.system("python main.py")
 
+#### Standalone Code
+
 def artemis_help():
+    print("")
     withwhat = input("""With what do you need help?
 1. App Issue/Bug
 2. How to use
-3. Go back""")
+3. Credits
+4. Go back""")
+    
     if withwhat == "1":
-        print("")
         print("Please report any bug at https://github.com/vortexsys/artemis/issues")
         input("Press enter to continue")
-        (clear)
+        clear()
         artemis_help()
+
     elif withwhat == "2":
         print("")
         print("Soon")
         input("Press enter to continue")
-        (clear)
-        artemis_help()
-    elif withwhat == "3":
-        restart()
-    else:
-        input("Wrong input! Press Enter to try again")
         clear()
         artemis_help()
+
+    elif withwhat == "3":
+        print("Made by https://github.com/vortexsys/")
+        print("Official Project repository: https://github.com/vortexsys/artemis")
+        
+    elif withwhat == "4":
+        clear()
+        restart()
+    else:
+        print("Invalid input!")
+        artemis_help()
+
 
 def artemis():
      print("Logging in on:",today,", what a beautiful day!")
@@ -54,15 +66,17 @@ def artemis():
 3. About
 4. Exit the program
 [>] """)
+     
      if whattodo == "1":
-         os.system('python menu.py')
+         os.system('python ./python/menu.py')
          clear()
+
      elif whattodo == "2":
          artemis_help()
+
      elif whattodo == "3":
-         print("")
          input("Soon")
-         restart()
+
      elif whattodo == "4":
          exit()
 artemis()
